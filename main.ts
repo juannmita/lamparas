a083561cd12f39b09cc9977dab00fa1c92332f9b
juannmita <154411350+juannmita@users.noreply.github.com>
2023-12-20 08:25:37 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.showNumber(input.lightLevel())
+basic.pause(5000)
+loops.everyInterval(500, function () {
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
